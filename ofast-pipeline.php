924355e6b6cca40987580df27a4caf9accc49b80
Olabode <101191848+Ofastmobile@users.noreply.github.com>
2026-08-13 08:27:52 +0100

    fix: run commerce migration after schema creation

--- ofast-pipeline.php
+++ ofast-pipeline.php
@@ -87,8 +87,8 @@ add_action( 'plugins_loaded', function (): void {
     new OFP_Cron_Handler();
     new OFP_Property_CPT();
     OFP_Host_Router::init();
-    OFP_Property_Commerce_Migration::init();
     OFP_Property_Commerce::init();
+    OFP_Property_Commerce_Migration::init();
     OFP_Property_Sales::init();
     OFP_Property_Sales_Client_UI::init();
 } );
